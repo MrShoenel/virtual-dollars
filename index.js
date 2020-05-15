7321cf4e96c527dcf5a6586d044085689e6849b3
Sebastian Hönel <sebastian.honel@lnu.se>
2020-05-15 11:24:45 +0200

    Add logging and option to specify names on command line

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const program = require('commander')
 , package = JSON.parse(fs.readFileSync(packagePath))
 , { NameMapper, JaroWinklerSimilarityScorer, LevenshteinSimilarityScorer } = require('./lib/NameMapper')
 , { AggregateReport } = require('./lib/AggregateReport')
-, { createObjectCsvWriter } = require('csv-writer');
+, { createObjectCsvWriter } = require('csv-writer')
+, { ColoredConsoleLogger, LogLevel } = require('sh.log-client');
 
 
 program
@@ -14,15 +15,18 @@ program
   .option('-m, --name-matcher <nameMatcher>', `Optional. The similarity measure to use. Options are '${JaroWinklerSimilarityScorer.name}' and '${LevenshteinSimilarityScorer.name}'. Default is '${JaroWinklerSimilarityScorer.name}'.`, JaroWinklerSimilarityScorer.name)
   .option('-g, --group <group>', 'The name of a group. If used, files with that group are grabbed, and the members in these files\' names are used. This is only used for initializing an authoritative list of names.', null)
   .option('-w, --week <week>', 'The number of a week. If used, files with that week are grabbed, and the members in these files\' names are used. This is only used for initializing an authoritative list of names.', null)
+  .option('-u, --use-names <useNames>', 'Optional. Comma-separated list of authoritative names. If present, these names will be used, instead of trying to grab them from files\' names.', '')
+  .option('-l, --log-level <logLevel>', 'Optional. The log-level. Defaults to Debug. Needs to be numeric (0-6), where 0 is the most verbose.', LogLevel.Debug)
   .requiredOption('-d, --dir <dir>', 'Required. Path to a directory with reports.')
   .requiredOption('-o, --out-file <outFile>', 'Required. The path to an output file. CSV will be written there.')
   .parse(process.argv);
 
-const sim = program.nameMatcher === JaroWinklerSimilarityScorer.name ?
+
+const logger = new ColoredConsoleLogger('Program')
+, sim = program.nameMatcher === JaroWinklerSimilarityScorer.name ?
     new JaroWinklerSimilarityScorer() : new LevenshteinSimilarityScorer()
 , nm = new NameMapper(sim, program.similarity)
-    .addNames(NameMapper.readNamesFromFilesInFolder(program.dir, program.week, program.group))
-, agg = new AggregateReport(nm).addReportsFromFolder(program.dir)
+, agg = new AggregateReport(nm)
 , csvW = createObjectCsvWriter({
     path: program.outFile,
     header: [{
@@ -46,13 +50,38 @@ const sim = program.nameMatcher === JaroWinklerSimilarityScorer.name ?
     }]
 });
 
+logger.logLevel = parseInt(program.logLevel, 10);
+logger.logInfo(`Similarities are measured using ${sim.__proto__.constructor.name}`);
+logger.logInfo(`Writing to file: ${program.outFile}`);
+
+if (program.useNames) {
+    logger.logInfo(`Using names: ${program.useNames}`);
+    nm.addNames(program.useNames.split(',').map(n => n.trim()));
+} else {
+    logger.logInfo(`Reading names from files: ${program.dir}, week: ${program.week === null ? 'null' : program.week}, group: ${program.group === null ? 'null' : program.group}`);
+    nm.addNames(NameMapper.readNamesFromFilesInFolder(
+        program.dir, program.week, program.group))
+}
+
+logger.logDebug(`The NameMapper uses these authoritative names: ${nm.names.join(', ')}`);
+
+
+
+try {
+    agg.addReportsFromFolder(program.dir);
+    logger.logInfo(`Aggregating ${agg.reports.length} reports.`);
+} catch (e) {
+    logger.logError(`Error reading reports from files in directory: ${program.dir}.`, e);
+    process.exit(-1);
+}
+
 
 
 const timeout = setTimeout(() => {}, 2**30);
 (async() => {
     try {
         for (const report of agg.reports) {
-            await csvW.writeRecords(report.records.map(rc => {
+            const records = report.records.map(rc => {
                 return {
                     member: rc.name,
                     week: report.week,
@@ -61,7 +90,11 @@ const timeout = setTimeout(() => {}, 2**30);
                     scrumsMissed: rc.scrumsMissed,
                     otherMissed: rc.otherMissed
                 }
-            }));
+            });
+
+            await csvW.writeRecords(records);
+
+            logger.logDebug(`Wrote ${records.length} Records of Report by ${report.author} (week: ${report.week}, group: ${report.group})`);
         }
     } finally {
         clearTimeout(timeout);
