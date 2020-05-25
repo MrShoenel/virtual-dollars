34dfd64509f3c4731abac499edab94ebc1c0c7eb
Sebastian Hönel <development@hoenel.net>
2020-05-25 11:59:18 +0200

    Make create-report.R create reports in parallel

--- analysis/create-report.R
+++ analysis/create-report.R
@@ -21,17 +21,46 @@ rmdReport <- normalizePath(paste0("./report_", noExt, ".Rmd"), mustWork = FALSE)
 base::writeLines(rmd, file(rmdReport))
 
 
-requiredPackages <- c("rmarkdown", "ggplot2", "dplyr")
+requiredPackages <- c("rmarkdown", "ggplot2", "dplyr", "parallel", "foreach", "doSNOW")
 for (rp in requiredPackages) {
-  if (!(rp %in% rownames(installed.packages()))) {
+  if (!(rp %in% rownames(suppressWarnings(suppressMessages(installed.packages()))))) {
     install.packages(rp)
   }
   if (!(rp %in% .packages())) {
-    library(rp, character.only = T)
+    suppressWarnings(suppressMessages(library(rp, character.only = T)))
   }
 }
 
 
-rmarkdown::render(rmdReport, output_dir = normalizePath("./output"), output_format = "all", output_file = rep(noExt, 4))
+renderFormats = c("pdf_document", "word_document", "md_document", "html_document")
 
-base::unlink(rmdReport)
+cl <- parallel::makeCluster(length(renderFormats))
+doSNOW::registerDoSNOW(cl)
+
+tryCatch({
+  pb <- txtProgressBar(max = length(renderFormats), style = 3)
+  progress <- function(n) setTxtProgressBar(pb, n)
+  
+  reports <- foreach::foreach(
+    rf = renderFormats,
+    .combine = c,
+    .options.snow = list(progress = progress)
+  ) %dopar% {
+    rmarkdown::render(
+      rmdReport,
+      output_dir = normalizePath("./output"),
+      output_format = rf,
+      output_file = noExt)
+  }
+  
+  cat("\r\n\r\nCreated the following reports:\r\n\r\n")
+  print(reports)
+}, warning=function(cond) {
+  print(paste("A warning occured:", cond))
+}, error=function(cond) {
+  print(paste("An error occured:", cond))
+}, finally = {
+  parallel::stopCluster(cl)
+  foreach::registerDoSEQ()
+  base::unlink(rmdReport)
+})
